22f4401bc1ba0667cc3519d9dd3aaaab1c65ea5f
Tatiana-Hela <malya9472@gmail.com>
2023-04-05 18:56:52 +0300

    add comment

--- src/Screens/mainScreen/nestedScreens/CommentsScreen.jsx
+++ src/Screens/mainScreen/nestedScreens/CommentsScreen.jsx
@@ -1,9 +1,11 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const CommentsScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>CommentsScreen</Text>
+      <View>
+        <TextInput style={styles.input} placeholder="Comment..." />
+      </View>
     </View>
   );
 };
@@ -14,5 +16,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  input: {
+    fontSize: 16,
+    lineHeight: 19,
+    width:343,
+    marginHorizontal: 16,
+    background: "#F6F6F6",
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 100,
+    height: 50,
+    color: "#212121",
+    paddingBottom: 16,
+    paddingLeft: 16,
+    paddingTop:16,
   },
 });
